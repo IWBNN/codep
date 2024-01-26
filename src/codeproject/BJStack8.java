024388bd1ed167d15ceb717a9671e2af5604a471
IWBNN <deangeon@naver.com>
2024-01-26 17:00:05 +0900

    initial commit

--- src/codeproject/BJStack8.java
+++ src/codeproject/BJStack8.java
@@ -15,7 +15,7 @@ public class BJStack8 { //BJStack7 = BJS44
 
         int N = Integer.parseInt(br.readLine());
 
-        for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
 
             String[] s = br.readLine().split(" ");
 
