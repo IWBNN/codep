c3ff53bd08312e3b87294b60b883864be88a29da
IWBNN <deangeon@naver.com>
2024-02-06 17:43:26 +0900

    initial commit

--- src/bjsilver4/BJS42.java
+++ src/bjsilver4/BJS42.java
@@ -1,10 +1,10 @@
 package bjsilver4;
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 public class BJS42 { //1026
     public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
     Integer[] A = new Integer[N];
--- src/bjsilver4/BJS46.java
+++ src/bjsilver4/BJS46.java
@@ -1,33 +1,47 @@
 package bjsilver4;
 
 import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class BJS46 {
+public class BJS46 { // 1920 (시간초과)
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        
         int[] A = new int[N];
         int i = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(i = 0;i < N;i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
         int M = Integer.parseInt(br.readLine());
         int[] B = new int[M];
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         for(i = 0;i < N;i++){
-            B[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st2.nextToken());
         }
-
+        int sum = 0;
+        int count = 0;
         for(i = 0;i < N;i++){
             for(int j = 0;j < M;j++){
                 if (B[i] == A[j]){
-                    bw.write(B[i] + "\n");
+                    sum = 1;
+                    count =0;
+                    sb.append(1).append('\n');
+                    break;
+                } else{
+                    sum = 0;
+                    count++;
                 }
             }
+            if(count != 0){
+                sb.append(0).append('\n');
+            }
         }
+        bw.write(String.valueOf(sb));
         bw.flush();
         bw.close();
     }
